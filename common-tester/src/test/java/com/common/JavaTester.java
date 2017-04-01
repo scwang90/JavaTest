47b9b384ae2e62d2b9d3c05713b51733a7ab41c4
scwang90 <scwang90@hotmaiul.com>
2017-04-01 17:30:13 +0800

    添加 Jsoup 骑牛测试

--- common-tester/src/test/java/com/common/JavaTester.java
+++ common-tester/src/test/java/com/common/JavaTester.java
@@ -1,8 +1,12 @@
 package com.common;
 
+import org.junit.Test;
+
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.InputStream;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  *
@@ -24,4 +28,70 @@ public class JavaTester {
         FileInputStream view = ss.view(FileInputStream.class);
     }
 
+    @Test
+    public void testTime() {
+        System.out.println(System.currentTimeMillis());
+    }
+
+    public boolean isChinesePunctuation(char c) {
+        Character.UnicodeBlock ub = Character.UnicodeBlock.of(c);
+        System.out.println(ub);
+        return ub == Character.UnicodeBlock.GENERAL_PUNCTUATION
+                || ub == Character.UnicodeBlock.CJK_SYMBOLS_AND_PUNCTUATION
+                || ub == Character.UnicodeBlock.HALFWIDTH_AND_FULLWIDTH_FORMS
+                || ub == Character.UnicodeBlock.CJK_COMPATIBILITY_FORMS
+                || ub == Character.UnicodeBlock.VERTICAL_FORMS;
+    }
+
+    public boolean hasChinesePunctuation(String str) {
+        for (char c : str.toCharArray()) {
+            if (isChinesePunctuation(c)) {
+                return true;
+            }
+        }
+        return false;
+    }
+    //使用UnicodeBlock方法判断
+    public boolean isChineseBlock(char c) {
+        Character.UnicodeBlock ub = Character.UnicodeBlock.of(c);
+        if (ub == Character.UnicodeBlock.CJK_UNIFIED_IDEOGRAPHS
+                || ub == Character.UnicodeBlock.CJK_UNIFIED_IDEOGRAPHS_EXTENSION_A
+                || ub == Character.UnicodeBlock.CJK_UNIFIED_IDEOGRAPHS_EXTENSION_B
+                || ub == Character.UnicodeBlock.CJK_UNIFIED_IDEOGRAPHS_EXTENSION_C
+                || ub == Character.UnicodeBlock.CJK_UNIFIED_IDEOGRAPHS_EXTENSION_D
+                || ub == Character.UnicodeBlock.CJK_COMPATIBILITY_IDEOGRAPHS
+                || ub == Character.UnicodeBlock.CJK_COMPATIBILITY_IDEOGRAPHS_SUPPLEMENT) {
+            System.out.println(ub);
+            return true;
+        } else {
+            return false;
+        }
+    }
+    public boolean hasChineseBlock(String str) {
+        for (char c : str.toCharArray()) {
+            if (isChinesePunctuation(c)) {
+                return true;
+            }
+        }
+        return false;
+    }
+    @Test
+    public void testChar() {
+        String value = "，";
+        System.out.println(hasChinesePunctuation(value));
+        System.out.println(hasChineseBlock(value));
+    }
+    @Test
+    public void testChars() {
+        Set<Character.UnicodeBlock> set = new HashSet<>();
+        String value = "!@#$%^&*()_+-={}[];'\\:\"|<>?,./`~" +
+                "！@#￥%……&*（）——+=-0987654321【】{}；‘、：“|，。/《》？·~";
+        for (char c : value.toCharArray()) {
+            System.out.printf("%c - %s\n", c, Character.UnicodeBlock.of(c));
+            set.add(Character.UnicodeBlock.of(c));
+        }
+        for (Character.UnicodeBlock block : set) {
+            System.out.println("Character.UnicodeBlock." + block);
+        }
+    }
 }
